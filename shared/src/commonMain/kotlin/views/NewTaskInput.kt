48284433e0ac53fe8fc8259c63d3c2b61f64af89
Zhdanov Nikolai <nvzhdanov@alfabank.ru>
2023-10-10 14:43:09 +0400

    added closing keyboard by add new todo

--- shared/src/commonMain/kotlin/views/NewTaskInput.kt
+++ shared/src/commonMain/kotlin/views/NewTaskInput.kt
@@ -15,21 +15,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 
 @Composable
 fun NewTaskInput(onEnter: (text: String) -> Unit) {
     var newTodoTitle by remember { mutableStateOf("") }
+    var isError by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
     fun send() {
-        onEnter(newTodoTitle)
-        newTodoTitle = ""
+        if (newTodoTitle.isNotBlank()) {
+            onEnter(newTodoTitle)
+            newTodoTitle = ""
+            focusManager.clearFocus()
+            isError = false
+        } else {
+            isError = true
+        }
     }
 
     Row {
         TextField(newTodoTitle,
             modifier = Modifier.weight(1.0f),
             singleLine = true,
+            isError = isError,
             placeholder = @Composable {
                 Text("Enter you task")
             },

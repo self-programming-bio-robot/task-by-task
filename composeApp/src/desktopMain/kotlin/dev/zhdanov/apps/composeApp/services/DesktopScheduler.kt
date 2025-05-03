e8010bbde5526462fa3114cada8a67df80a02f4f
Zhdanov Nikolai <nvzhdanov@alfabank.ru>
2025-05-04 00:00:48 +0400

    fix: Changed implementation of desktop scheduler to run tasks after wakeup application

--- composeApp/src/desktopMain/kotlin/dev/zhdanov/apps/composeApp/services/DesktopScheduler.kt
+++ composeApp/src/desktopMain/kotlin/dev/zhdanov/apps/composeApp/services/DesktopScheduler.kt
@@ -1,19 +1,29 @@
 package dev.zhdanov.apps.composeApp.services
 
 import com.ucasoft.kcron.Cron
+import com.ucasoft.kcron.core.builders.Builder
 import com.ucasoft.kcron.core.builders.DelicateIterableApi
 import com.ucasoft.kcron.core.common.WeekDays
-import kotlinx.coroutines.*
-import kotlinx.datetime.Clock
+import com.ucasoft.kcron.kotlinx.datetime.CronLocalDateTime
+import com.ucasoft.kcron.kotlinx.datetime.CronLocalDateTimeProvider
+import kotlinx.datetime.*
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
-import org.lighthousegames.logging.KmLog
 import org.lighthousegames.logging.logging
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.minutes
 
 class DesktopScheduler(
-    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
-): SchedulerService {
+    private val executor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+) : SchedulerService {
+
+    private val schedulers = PriorityQueue<Scheduler> { s1, s2 -> s1.nextRun.compareTo(s2.nextRun) }
+
+    init {
+        loop()
+    }
 
     @OptIn(DelicateIterableApi::class)
     override fun addScheduler(tag: String, cron: String, timeZone: TimeZone, action: SchedulerAction) {
@@ -21,25 +31,55 @@ class DesktopScheduler(
             it.firstDayOfWeek = WeekDays.Monday
         }
 
-        val iterator = cronBuilder.asIterable(Clock.System.now().toLocalDateTime(timeZone)).iterator()
-
-        coroutineScope.launch {
-            while (this.isActive && iterator.hasNext()) {
-                val next = iterator.next()
-                logger.d { "Next scheduling '$tag' at $next" }
-                val duration = next.toInstant(timeZone).minus(Clock.System.now())
-                delay(duration)
-                logger.i { "Run scheduler: '$tag'" }
-                action.invoke()
-            }
-        }
+        addRun(cronBuilder, tag, timeZone, action)
     }
 
     override fun addScheduler(tag: String, cron: String, action: SchedulerAction) {
         this.addScheduler(tag, cron, TimeZone.currentSystemDefault(), action)
     }
 
+    private fun loop() {
+        executor.scheduleAtFixedRate(
+            {
+                if (schedulers.isNotEmpty()) {
+                    val scheduler = schedulers.peek()
+                    val duration = scheduler.nextRun.minus(Clock.System.now())
+
+                    if (duration.isNegative()) {
+                        schedulers.poll()
+                        logger.i { "Run scheduler: '${scheduler.tag}'" }
+
+                        addRun(scheduler.cron, scheduler.tag, scheduler.timeZone, scheduler.action)
+                        scheduler.action.invoke()
+                    }
+                }
+            },
+            0L, 1L, TimeUnit.SECONDS
+        )
+    }
+
+    private fun addRun(
+        cronBuilder: Builder<LocalDateTime, CronLocalDateTime, CronLocalDateTimeProvider>,
+        tag: String,
+        timeZone: TimeZone,
+        action: SchedulerAction
+    ) {
+        cronBuilder.nextRun?.let { next ->
+            schedulers.add(Scheduler(tag, cronBuilder, next.toInstant(timeZone), timeZone, action))
+            val scheduler = schedulers.peek()
+            logger.d { "Next run: '${scheduler.tag}' at ${scheduler.nextRun}" }
+        }
+    }
+
     companion object {
         private val logger = logging(DesktopScheduler::class.qualifiedName)
     }
+
+    data class Scheduler(
+        val tag: String,
+        val cron: Builder<LocalDateTime, CronLocalDateTime, CronLocalDateTimeProvider>,
+        val nextRun: Instant,
+        val timeZone: TimeZone,
+        val action: SchedulerAction
+    )
 }
